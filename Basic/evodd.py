4dbe1ae5029332c18644353d00fcd40162496eb4
ABHISIKTA GHOSH <abhisiktaghosh2@gmail.com>
2025-09-12 08:54:59 +0530

    Add files via upload

---
+++ Basic/evodd.py
@@ -0,0 +1,15 @@
+#program to print a natural nos. find odd even
+a=int(input("enter the value of a"))
+print("Natural numbers are: ")
+sum=0;
+for i in range(1,a+1):
+    print(i)
+print("sum is: ")
+for i in range(1,a+1):
+    sum+=i
+print(sum)
+for i in range(1,a+1):
+    if(i%2==0):
+        print(i,"is even")
+    else:
+        print(i,"is odd")
